3b9db0548f76b24feabbb04b6f2b4b8212a97bf3
Diaz Leonata <diazleonata@outlook.com>
2025-10-04 08:16:26 +0000

    initial

---
+++ python/latihan1.py
@@ -0,0 +1,16 @@
+# print("Latihan 1", 26, "data", 96.75)
+# print("Nama saya", nama, ", umur saya", umur, "tahun, dan tinggi saya", tinggi, "cm.")
+
+# nama = "Diaz"
+# umur = 15
+# tinggi = 145
+# x = umur + tinggi
+# y = umur * tinggi
+# z = tinggi // umur
+# print("Jumlah umur ditambah tinggi:", x)
+# print("Jumlah umur dikali tinggi:", y)
+# print("Jumlah tinggi dibagi umur:", z)
+
+# print(umur > 20 and tinggi > 100)
+
+print((6 + 5) - (6 + 3))
