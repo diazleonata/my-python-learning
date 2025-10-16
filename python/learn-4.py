38953653a71245e5f4d8ed9e976435c109c21708
Diaz Leonata <diazleonata@outlook.com>
2025-10-16 07:40:25 +0000

    update

---
+++ python/learn-4.py
@@ -0,0 +1,15 @@
+# loop
+# x = 1
+# while x < 100:
+#     print(x)
+#     x += 1
+
+# loop with ganjil genap
+x = 1
+
+while x < 100:
+    if x % 2 == 0:
+        print(f"{x} adalah bilangan genap")
+    else:
+        print(f"{x} adalah bilangan ganjil")
+    x += 1 
