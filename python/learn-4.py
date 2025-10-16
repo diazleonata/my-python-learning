bbc61e101937cd5e4345958fe0ed6055a869f248
Diaz Leonata <diazleonata@outlook.com>
2025-10-16 08:25:11 +0000

    update

--- python/learn-4.py
+++ python/learn-4.py
@@ -5,11 +5,27 @@
 #     x += 1
 
 # loop with ganjil genap
+# x = 1
+
+# while x < 100:
+#     if x % 2 == 0:
+
+#         print(f"{x} adalah bilangan genap")
+#     else:
+#         print(f"{x} adalah bilangan ganjil")
+#     x += 1 
+
+# loop with function
+def ganjilGenap(x):
+    if x % 2 == 0:
+        ket = "Bilangan Genap"
+    else:
+        ket = "Bilangan Ganjil"
+    return ket
+
 x = 1
 
-while x < 100:
-    if x % 2 == 0:
-        print(f"{x} adalah bilangan genap")
-    else:
-        print(f"{x} adalah bilangan ganjil")
-    x += 1 
+while x < 101:
+    ket = ganjilGenap(x)
+    print(f"{x} adalah {ket}")
+    x += 1
